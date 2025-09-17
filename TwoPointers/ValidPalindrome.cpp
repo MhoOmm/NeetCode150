808a14b71333974e1e3c897032831576b98918f5
Mho <ommtripathi046@gmail.com>
2025-09-17 23:00:48 +0530

    vp

---
+++ TwoPointers/ValidPalindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string str) {
+
+        string s;
+        
+        
+        for(char c : str) {
+            if(isalnum(c)) {
+                s.push_back(tolower(c));
+            }
+        }
+
+        int i= 0;
+        int j = s.length()-1;
+
+        while(i<=j)
+        {
+            if(s[i]!=s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
