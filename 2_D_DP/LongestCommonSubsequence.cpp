005331ca5e9b6fdb3b4928ab29d21734de2bdfd2
Mho <ommtripathi046@gmail.com>
2025-09-22 22:08:20 +0530

    lcs

--- 2_D_DP/LongestCommonSubsequence.cpp
+++ 2_D_DP/LongestCommonSubsequence.cpp
@@ -1,3 +1,4 @@
+// recursion
 #include<cstring>
 class Solution {
 public:
@@ -27,5 +28,37 @@ public:
         m = text2.size();
         memset(t,-1,sizeof(t));
         return solve(0,0,text1,text2);
+    }
+};
+
+// bottom up 
+#include<cstring>
+class Solution {
+public:
+    int n,m;
+    int longestCommonSubsequence(string text1, string text2) {
+        n = text1.size();
+        m = text2.size();
+        int t[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            t[i][0]=0;
+        }
+        for(int i=0;i<=m;i++)
+        {
+            t[0][i] = 0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1])
+                {
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]= max(t[i][j-1],t[i-1][j]);
+                }
+            }
+        }
+        return t[n][m];
     }
 };
