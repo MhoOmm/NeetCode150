fe3b21013a6392ae1c53f3d40fcc6e8552e91b9c
Mho <ommtripathi046@gmail.com>
2025-09-30 11:23:49 +0530

    adh

--- 1_D_DP/LongestIncreasingSubsequence.cpp
+++ 1_D_DP/LongestIncreasingSubsequence.cpp
@@ -33,3 +33,29 @@ public:
         return solve(nums,0,-1);
     }
 };
+
+
+// bottom up approach
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int>t(n,1);
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    t[i] = max(t[i],1+t[j]);
+                }
+            } 
+        }
+
+        return *max_element(begin(t),end(t));
+        
+    }
+};
+
