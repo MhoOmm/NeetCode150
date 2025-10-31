ff6435a5232b838795630ed3bd95c2dc21cb1bc4
Mho <ommtripathi046@gmail.com>
2025-11-01 02:37:23 +0530

    TargetSum

---
+++ 2_D_DP/TargetSum.cpp
@@ -0,0 +1,32 @@
+// top down approach
+class Solution {
+public:
+    int t[1001][2001];
+    int solve(vector<int>&nums,int i,int sum,int target,unordered_map<string,int>&mp)
+    {
+        if(i==nums.size())
+        {
+            if(sum==target)
+            {
+               return 1;
+            }else{return 0;}
+        }
+        string key = to_string(i)+"_"+to_string(sum);
+
+        if(mp.count(key))
+        {
+            return mp[key];
+        }
+
+        int plus = solve(nums,i+1,sum+nums[i],target,mp);
+        int minus = solve(nums,i+1,sum-nums[i],target,mp);
+
+        return mp[key] = plus+minus;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int n= nums.size();
+        unordered_map<string,int>mp;
+        return solve(nums,0,0,target,mp);
+    }
+};
