36a34d3b2d52db65397ca39913add6f96633a9a9
Mho <ommtripathi046@gmail.com>
2025-11-07 09:07:30 +0530

    kth-largest-element-in-an-array

---
+++ Heap&PriorityQueue/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq(nums.begin(),nums.end());
+
+        int ans=0;
+
+        while(k)
+        {
+            ans = pq.top();
+            pq.pop();
+            k--;
+        }
+
+        return ans;
+        
+    }
+};
