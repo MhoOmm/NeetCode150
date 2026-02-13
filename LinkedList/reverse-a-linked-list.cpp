51c457eefab7e442f5dbf172d31171f36ca1af9c
Mho <ommtripathi046@gmail.com>
2026-02-13 19:21:04 +0530

    reverse a linked list

---
+++ LinkedList/reverse-a-linked-list.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* last;
+    ListNode* solve(ListNode* head)
+    {
+        if(head==nullptr || head->next==nullptr)
+        {
+            return head;
+        }
+        last = solve(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return last;
+    }
+    ListNode* reverseList(ListNode* head) {
+        last = solve(head);
+        return last; 
+    }
+};
