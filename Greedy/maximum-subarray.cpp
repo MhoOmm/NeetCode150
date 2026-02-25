ddd5e1cbdd8eb5e9e1eafd4795b5e4f73130a314
Mho <ommtripathi046@gmail.com>
2026-02-25 20:37:43 +0530

    maximum-subarray

---
+++ Greedy/maximum-subarray.cpp
@@ -0,0 +1,16 @@
+// Applying Kadane's Algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i=1;i<n;i++)
+        {
+            currSum = max(nums[i],currSum+nums[i]);
+            maxSum = max(currSum,maxSum);
+        }
+        return maxSum;
+    }
+};
